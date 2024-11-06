f003a62b8c2c283c1190d674031b1ac1319352a8
Vanillatte68 <roselinyosefa@yahoo.com>
2024-11-06 22:30:38 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -4,23 +4,125 @@ import CreateUser from "./components/createUser";
 import { fetchMultipleGithubUsers } from "./components/githubAPI";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  // mock data
+  const [usernames, setUsernames] = useState([
+    "mojombo",
+    "defunkt",
+    "pjhyett",
+    "wycats",
+    "evanphx",
+    "linghu",
+    "brynary",
+    "davetang",
+  ]);
+
+  // fetch github users start
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const userDataArray = await fetchMultipleGithubUsers(usernames);
+      const userObject = userDataArray.map((user) => ({
+        id: user.id,
+        name: user.name,
+        email: `${user.login}@gmail.com`,
+        age: Math.floor(Math.random() * (60 - 19) + 19),
+        status: user.side_admin ? "Active" : "Inactive",
+      }));
+      setUsers(userObject);
+    };
+    fetchUsers();
+  }, [usernames]);
+  // fetch github user end
+
+  // render new user start
+  const [newUser, setNewUser] = useState([]);
+  useEffect(() => {
+    const storedUsers = JSON.parse(localStorage.getItem("users") || "[]");
+    setNewUser(storedUsers);
+  }, []);
+  // render new user end
+
   return (
-    <div className="App">
+    <>
+      {/* header & title*/}
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>DataTech</h1>
       </header>
-    </div>
+
+      {/* main content start */}
+
+      <main>
+        {/* filter, sort, new user handler */}
+        <div>
+          <form>
+            <input type="search" id="query" placeholder="Search..." />
+            <button>Search</button>
+          </form>
+          <button>Sort</button>
+        </div>
+
+        {/* githubAPI users table start */}
+        <h2>Github Users</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Age</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* map user by id */}
+            {users.map((user) => (
+              <tr key={user.id}>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.age}</td>
+                <td>{user.status}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        {/* githubAPI users table end */}
+
+        {/* new user table start */}
+        <h2>New Users</h2>
+        <button
+          id="open-dialog"
+          onClick={() => document.querySelector("dialog").showModal()}
+        >
+          Add User
+        </button>
+        <dialog>
+          <CreateUser />
+        </dialog>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Age</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {newUser.map((user) => (
+              <tr key={user.id}>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.age}</td>
+                <td>{user.status}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        {/* new user table end */}
+      </main>
+
+      {/* main content end */}
+    </>
   );
 }
 
